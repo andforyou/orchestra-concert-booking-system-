23ef3ca71fda4ce4c5b88e884961d2ee55e3e8ce
Jerrie Yang <jingwen.yang-1@student.uts.edu.au>
2025-05-11 22:28:19 +1000

    Initial commit of Xcode project

---
+++ BooooooooookingSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BooooooooookingSystem
+//
+//  Created by 牙后慧 on 2025/5/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
