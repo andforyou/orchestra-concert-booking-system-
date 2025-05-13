70dd6f3a6c81f0a498de8f77c77786d25af61d27
TriPham1312 <phcongtri93@gmail.com>
2025-05-14 04:31:46 +1000

    use envrironment object from main

--- BooooooooookingSystem/BooooooooookingSystemApp.swift
+++ BooooooooookingSystem/BooooooooookingSystemApp.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 @main
 struct ConcertBookingSystemApp: App {
+    @StateObject var concertVM = ConcertViewModel()
+    @StateObject var bookingVM = BookingViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(concertVM)
+                .environmentObject(bookingVM)
         }
     }
 }
